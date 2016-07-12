d7eb5e2a5f60082ae9e8c35da444e89ee5ab4404
Rada Bogdan Raul <raoul_ice@yahoo.com>
2016-07-12 13:27:37 +0300

    remove useless actor

--- lib/capistrano_sentinel/all.rb
+++ lib/capistrano_sentinel/all.rb
@@ -21,10 +21,13 @@ require_relative './classes/gem_finder'
   Gem.find_files("#{CapistranoSentinel::GemFinder.get_current_gem_name}/#{folder_name}/**/*.rb").each { |path| require path }
 end
 
-if !CapistranoSentinel::GemFinder.value_blank?(ENV[CapistranoSentinel::RequestHooks::ENV_KEY_JOB_ID]) && CapistranoSentinel::GemFinder.fetch_gem_version('capistrano')
-  if CapistranoSentinel::GemFinder.capistrano_version_2?
-    require_relative './patches/capistrano2'
-  else
-    require_relative './patches/rake'
+if !CapistranoSentinel::GemFinder.value_blank?(ENV[CapistranoSentinel::RequestHooks::ENV_KEY_JOB_ID])
+
+  if CapistranoSentinel::GemFinder.fetch_gem_version('capistrano')
+    if CapistranoSentinel::GemFinder.capistrano_version_2?
+      require_relative './patches/capistrano2'
+    else
+      require_relative './patches/rake'
+    end
   end
 end
